f89c8a46a07ba1d4927e4b555ef4e9f8c76e0f10
Yathin A <130223617+Yathinvaibhav@users.noreply.github.com>
2023-05-07 10:11:23 +0530

    Update ca1.js

--- ca1.js
+++ ca1.js
@@ -34,20 +34,12 @@ tempLoad();
 
 const calculateTemp = () => {
     const numberTemp = document.getElementById('temp').value;
-    // console.log(numberTemp);
-
     const tempSelected = document.querySelector('#temp_diff');
     const valeTemp = temp_diff.options[tempSelected.selectedIndex].value;
-    // console.log(valeTemp);
-
-
-    // Convert temperature from Celcius to Fahrenheit
     const celTOfah = (cel) => {
         let fahrenheit = (cel * (9 / 5) + 32);
         return fahrenheit;
     }
-
-    // Convert temperature from Fahrenheit to Celsius
     const fahTOcel = (fehr) => {
         let celsius = ((fehr - 32) * 5 / 9);
         return celsius;
